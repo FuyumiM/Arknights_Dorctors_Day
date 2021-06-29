f3df706c98c71e4501844c76798f13d87e15650d
FuyumiM <Irisu_M@outlook.com>
2021-06-29 17:40:35 +0800

    const test

--- js/system.js
+++ js/system.js
@@ -7,7 +7,7 @@ function main() {
 }
 
 function main_page() {
-    addElement(newElement("<div class='segmenting-line'><hr><div><span>神经连接中</span></div></div>"));
+    addElement(newElement(const_element["main_loading"]));
 
     addElement(newElement("<p style='float:center;text-align: center;'>为您接通助理线路...</p>"));
 
