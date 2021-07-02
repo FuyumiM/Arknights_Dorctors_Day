7582513bfec7516dada626e464f8c7ff3223c492
FuyumiM <Irisu_M@outlook.com>
2021-07-03 07:53:15 +0800

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -10,25 +10,25 @@ function newElement(text) {
 function newText(text) {
     var span = document.createElement('p');
     span.innerHTML = text;
-    span.style='text-indent: 2em;';
+    span.style = 'text-indent: 2em;';
     return span;
 }
 
-function addElement(e){
+function addElement(e) {
     post_body.appendChild(e);
 }
 
 function getBody() {
-    if(debug){
+    if (debug) {
         var box = document.getElementsByName('debug_body');
         return box[0];
-    }else{
-        var box = document.getElementsByClassName('jinsom-bbs-single-content');
-        if (box.length == 0) {
-            box = document.getElementsByClassName('jinsom-post-single-content');
+    } else {
+        var box = document.querySelector("body > div.jinsom-main-content.single-bbs.default.single.clear > div > div.jinsom-bbs-single-box.main.clear > div.right > div.jinsom-bbs-single-content");
+        if (box == null) {
+            box = document.querySelector("#jinsom-view-sns-0 > div.pages.navbar-through > div.page.no-tabbar.post-bbs.page-on-center > div.page-content.keep-toolbar-on-scroll.jinsom-page-single-content.jinsom-page-single-content-1306 > div.jinsom-single.jinsom-post-1306 > div.jinsom-post-single-content");
         }
-        if (box.length != 0) {
-            return box[0];
+        if (box != null) {
+            return box;
         }
     }
     return null;
