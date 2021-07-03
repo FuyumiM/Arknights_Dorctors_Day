c6ed3718573c0904983f6ac69c704368799bf0c2
FuyumiM <Irisu_M@outlook.com>
2021-07-03 10:00:54 +0800

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -23,15 +23,9 @@ function getBody() {
         var box = document.getElementsByName('debug_body');
         return box[0];
     } else {
-        var box = document.querySelector("body > div.jinsom-main-content.single-bbs.default.single.clear > div > div.jinsom-bbs-single-box.main.clear > div.right > div.jinsom-bbs-single-content");
-        if (box == null) {
-            box = document.querySelector("#jinsom-view-sns-0 > div.pages.navbar-through > div.page.no-tabbar.post-bbs.page-on-center > div.page-content.keep-toolbar-on-scroll.jinsom-page-single-content.jinsom-page-single-content-1306 > div.jinsom-single.jinsom-post-1306 > div.jinsom-post-single-content");
-        }
-        if (box != null) {
-            return box;
-        }
+        var box = document.getElementsByClassName("fuyumi_anchor")[0].parentElement;
+        return box;
     }
-    return null;
 }
 
 function removeAllChild(e) {
